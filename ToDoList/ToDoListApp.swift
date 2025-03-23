c171981821d4ad367f23e4d0e195c4ca8dc53b17
Gabe Witcher <gabewitcher@gmail.com>
2025-03-22 20:15:33 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Gabe Witcher on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
